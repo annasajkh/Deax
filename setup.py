3bf9cb5a54de5a2ae7649cd46b449da67fbcb018
annasajkh <annaskherunas06@gmail.com>
2021-06-27 22:11:18 +0700

    add ai

--- setup.py
+++ setup.py
@@ -87,8 +87,8 @@ from helper import *
 
 data = {
     "inputs": {
-        "past_user_inputs": [],
-        "generated_responses": [],
+        "past_user_inputs": {},
+        "generated_responses": {},
         "text": "",
     },
 }
@@ -112,9 +112,9 @@ async def on_message(message : discord.Message):
         data["inputs"]["past_user_inputs"].append(text)
         data["inputs"]["generated_responses"].append(response)
 
-        if len(data["inputs"]["past_user_inputs"]) > 3:
+        if len(data["inputs"]["past_user_inputs"]) > 10000:
             data["inputs"]["past_user_inputs"].pop(0)
-            data["inputs"]["generated_responses"] = []
+            data["inputs"]["generated_responses"].pop(0)
         
         print(data["inputs"]["past_user_inputs"])
         print(data["inputs"]["generated_responses"])
