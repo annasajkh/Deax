d28797d4d4c4aea7930034d44f8e816ad883246d
annasajkh <annaskherunas06@gmail.com>
2021-06-28 22:19:07 +0700

    remove unused code

--- bot_client.py
+++ bot_client.py
@@ -5,7 +5,7 @@ import discord
 
 class BotClient(BotBase, discord.Client):
     previous_message = ""
-
+    
     async def on_ready(self):
         print(f"Logged on as {self.user}")
 
--- commands.py
+++ commands.py
@@ -15,7 +15,6 @@ import os
 import wikipedia
 import fandom
 import requests
-import simpleeval
 
 
 @bot.command()
