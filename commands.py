68afc28765378c419ac7673110b29ec8940378ee
annasajkh <annaskherunas06@gmail.com>
2021-07-07 13:36:15 +0700

    synth comeback yay now i will set the delay to 25 seconds i hope it doesnt crash

--- commands.py
+++ commands.py
@@ -328,6 +328,7 @@ async def nt(ctx, img_url=""):
 async def tg(ctx, *, text):
     try:
         async with ctx.typing():
+            
             await asyncio.sleep(10)
 
             browser, page, input_text, submit_button = await setup_browser()
