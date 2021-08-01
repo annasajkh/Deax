5e48e888bacd0aba71e408eeb6dcfc798b731308
annasajkh <annaskherunas06@gmail.com>
2021-08-01 16:37:40 +0700

    last

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
 from commands import bot
-from threading import Thread
-
-from flask import Flask
 import os
-
-app = Flask('')
-
-@app.route('/')
-def main():
-  return "your bot is alive"
-
-
-def run():
-    app.run(host="0.0.0.0", port=8080)
-
-def keep_alive():
-    server = Thread(target=run)
-    server.start()
-
-keep_alive()
-
 
 bot.run(os.environ["BOT_TOKEN"])
