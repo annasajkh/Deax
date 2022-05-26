47fa2716dbf7cba8d185eede30f877e6e4c91b0f
annasajkh <annaskherunas06@gmail.com>
2022-05-26 16:43:14 +0700

    fix

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ if not exists("cc_captions.txt"):
 
 
 client = Client("https://demo-cas.jina.ai:8443")
-captions = open("others.txt", "r").read().split("\n") + open("cc_captions.txt", "r").read().split("\n")[20_000]
+captions = open("others.txt", "r").read().split("\n") + open("cc_captions.txt", "r").read().split("\n")[:20_000]
 
 encoded = None
 
