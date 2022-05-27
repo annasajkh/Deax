c7ee16bba8a1c11d2c1f8174398372cc1a7980d5
annasajkh <annaskherunas06@gmail.com>
2022-05-27 16:33:28 +0700

    fix

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ def ignore_errors(f):
 
 def generate_dalleflow(prompt):
     da = Document(text=prompt).post("grpc://dalle-flow.jina.ai:51005", parameters={"num_images": 1}).matches
-    da = da[0].post("grpc://dalle-flow.jina.ai:51005", parameters={"skip_rate": 0.6, "num_images": 1}, target_executor="diffusion").matches
+    da = da[0].post("grpc://dalle-flow.jina.ai:51005", parameters={"skip_rate": 0.9, "num_images": 1}, target_executor="diffusion").matches
     da = da[0].post("grpc://dalle-flow.jina.ai:51005/upscale")
     da.save_uri_to_file("image.png")
 
